8d05b1e8829d6e38db0f443d62b027560710a3bc
Suraj Shinde <surajpshinde8767@gmail.com>
2026-04-05 14:05:18 +0530

    New microservices examples

--- RESTful Web Services/restful-web-services/src/main/java/com/microservices/rest/web_services/modal/User.java
+++ RESTful Web Services/restful-web-services/src/main/java/com/microservices/rest/web_services/modal/User.java
@@ -1,5 +1,9 @@
 package com.microservices.rest.web_services.modal;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -11,14 +15,19 @@ import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "User_Details")
 public class User {
 
+    @Id
+    @GeneratedValue
     private int id;
 
     @Size(min = 2, message = "Name Should Have at Least 2 Characters")
+    @JsonProperty("user_name")
     private String name;
 
     @Past(message = "Birth Date should be in past")
+    @JsonProperty("birth_date")
     private LocalDate birthDate;
 
 }
